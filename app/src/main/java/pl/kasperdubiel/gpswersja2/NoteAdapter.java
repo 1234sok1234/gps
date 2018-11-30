8c7c2e3800f6624bab780b793558df2998d4cca0
Corsair V8 <Corsair V8@DESKTOP-JLQ6B1T>
2018-11-30 20:27:23 +0100

    no to teraz gps z poprawkami do stringow

--- app/src/main/java/pl/kasperdubiel/gpswersja2/NoteAdapter.java
+++ app/src/main/java/pl/kasperdubiel/gpswersja2/NoteAdapter.java
@@ -1,6 +1,8 @@
 package pl.kasperdubiel.gpswersja2;
 
 import android.support.annotation.NonNull;
+import android.support.v7.recyclerview.extensions.ListAdapter;
+import android.support.v7.util.DiffUtil;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +12,28 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder>
+public class NoteAdapter extends ListAdapter<Note,NoteAdapter.NoteHolder>
 {
-	private List<Note> notes = new ArrayList<>();
+	public NoteAdapter()
+	{
+		super(DIFF_CALLBACK);
+	}
+	private static final DiffUtil.ItemCallback<Note> DIFF_CALLBACK=new DiffUtil.ItemCallback<Note>()
+	{
+		@Override
+		public boolean areItemsTheSame(@NonNull Note oldItem, @NonNull Note newItem)
+		{
+			return oldItem.getId()==newItem.getId();
+		}
+
+		@Override
+		public boolean areContentsTheSame(@NonNull Note oldItem, @NonNull Note newItem)
+		{
+			return oldItem.getTitle().equals(newItem.getTitle()) &&
+					oldItem.getDescription().equals(newItem.getDescription()) &&
+					oldItem.getPriority() == newItem.getPriority();
+		}
+	};
 
 	@NonNull
 	@Override
@@ -24,19 +45,16 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder>
 
 	@Override
 	public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
-		Note currentNote = notes.get(position);
+		Note currentNote = getItem(position);
 		holder.textViewTitle.setText(currentNote.getTitle());
 		holder.textViewDescription.setText(currentNote.getDescription());
 		holder.textViewPriority.setText(String.valueOf(currentNote.getPriority()));
 	}
-	@Override
-	public int getItemCount() {
-		return notes.size();
-	}
 
-	public void setNotes(List<Note> notes) {
-		this.notes = notes;
-		notifyDataSetChanged();
+
+	public Note getNoteAt(int positon)
+	{
+		return getItem(positon);
 	}
 	class NoteHolder extends RecyclerView.ViewHolder
 	{
